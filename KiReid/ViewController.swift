d9f4a9540d237e57e0af8b321d1240111ccd8c8f
Kiya Eshaghian <kiyaeshghian@gmail.com>
2015-11-01 13:05:51 -0800

    KiReid

---
+++ KiReid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KiReid
+//
+//  Created by Kiya Eshaghian on 11/1/15.
+//  Copyright © 2015 Kiya Eshaghian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
